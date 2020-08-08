1ede64e5d0721c6ac6a3d58e2b8fac3942b792ff
QuangMinh1912 <minhminhminhminh1912@gmail.com>
2020-08-08 12:36:01 +0700

    QLDH commit

---
+++ QLDH/QLDH/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDH.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult OrdersList()
+        {
+            return View();
+        }
+        public ActionResult OrderConfirmation()
+        {
+            return View();
+        }
+        public ActionResult OrderInformation()
+        {
+            return View();
+        }
+        public ActionResult ShipmentDetails()
+        {
+            return View();
+        }
+    }
+}
